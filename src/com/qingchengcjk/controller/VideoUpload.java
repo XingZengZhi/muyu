9a8408fa648dbd1dd837b061663a1b9423afd1cc
XingZengZhi <944047118@qq.com>
2017-07-25 10:52:09 +0800

    更新

--- src/com/qingchengcjk/controller/VideoUpload.java
+++ src/com/qingchengcjk/controller/VideoUpload.java
@@ -7,26 +7,25 @@ import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
 public class VideoUpload {
 	@RequestMapping("/upload")
-	public String videoupload(Model model, MultipartFile videoFile){
-		String message = "";
+	public String videoupload(HttpServletRequest request, 
+			MultipartFile videoFile){
+		//上传视频的路径
+		String uploadPath = request.getRealPath(File.separatorChar + "static" + File.separatorChar + "video");
 		//原始文件名
 		String video_name = videoFile.getOriginalFilename();
-		System.out.println(video_name);
 		//新文件名
 		String video_newName = UUID.randomUUID().toString() + video_name.substring(video_name.lastIndexOf("."));
-		String type = video_name.substring(video_name.lastIndexOf("."), video_name.length());
-		System.out.println(video_newName + " " + type);
+		System.out.println();
 		//上传视频
-		File uploadVideo = new File("E:/qingcheng/qingchengcjk/WebContent/static/video" + video_newName + type);
+		File uploadVideo = new File(uploadPath + File.separatorChar + video_newName);
 		if(!uploadVideo.exists()){
 			uploadVideo.mkdirs();
 		}
