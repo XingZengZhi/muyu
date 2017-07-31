4b331082396721e736050229c0d1f528896e8802
XingZengZhi <944047118@qq.com>
2017-07-31 23:26:53 +0800

    再优化...

--- src/com/qcys/controller/FormCotroller.java
+++ src/com/qcys/controller/FormCotroller.java
@@ -21,13 +21,9 @@ public class FormCotroller {
 	private UserService userService;
 	@RequestMapping("/loginForm")
 	public String loginForm(HttpServletRequest request, HttpServletResponse response) throws IOException{
-		//获得验证码
 		String k = (String) request.getSession().getAttribute(Constants.KAPTCHA_SESSION_KEY);
-		System.out.println(k);
 		String r = request.getParameter("r");
-		response.setCharacterEncoding("UTF-8");
 		if(k.equals(r)){
-			//验证成功，执行用户登录
 			String account = request.getParameter("account");
 			String password = request.getParameter("password");
 			User user = userService.UserLogin(account, password);
